bc103d360093f3cca7b138b02ac3cc0e0d65bfc9
tiewweijian <tiewweijian@u.nus.edu>
2025-08-08 15:23:50 +0800

    Intialise Springboot backend

---
+++ backend/src/main/java/com/nus/sellr/SellrApplication.java
@@ -0,0 +1,13 @@
+package com.nus.sellr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nus/sellr/SellrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.sellr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
